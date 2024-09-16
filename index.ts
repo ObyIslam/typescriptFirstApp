f3af9bc7643442c7d6b92bcba641b2963511f0fd
ObyIslam <S00235259@atu.ie>
2024-09-16 17:42:11 +0100

    finished

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, {Application, Request, Response} from "express";
+import morgan from "morgan";
+
+const PORT = process.env.PORT || 3000;
+
+const app: Application = express();
+
+app.use(morgan("tiny"));
+
+app.get("/ping", async (_req : Request, res: Response) => 
+    { 
+     res.send({ 
+     message: "hello from Oby", 
+     }); 
+}); 
+
+app.get('/bananas', async (_req : Request, res: Response)=>
+     res.send('hello world, this is bananas')); 
+
+app.listen(PORT);
+
